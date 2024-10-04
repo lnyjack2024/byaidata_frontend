ffaa5477f9c3c52dbd2e0dea25d03ab22dea5694
wyh20240819 <wangyonghong@byaidata.com>
2024-10-04 23:25:40 +0800

    操作员列表

--- src/pages/auth_management/user.jsx
+++ src/pages/auth_management/user.jsx
@@ -2,14 +2,72 @@
  * @Description: 
  * @Author: wangyonghong
  * @Date: 2024-09-30 20:42:03
- * @LastEditTime: 2024-09-30 20:42:31
+ * @LastEditTime: 2024-10-04 23:19:56
  */
-import React from 'react'
+import React , { useEffect } from 'react'
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Table } from 'antd'
+import './user.css'
 
 export default function User() {
+  const handClink = () => {
+
+  }
+  const hangFinish = (e) => {
+    console.log(e)
+  }
+  useEffect(() => {
+
+  },[])
+
+  const column = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '住址',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+  const data = [
+    {
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号',
+    },
+    {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号',
+    },
+  ];
   return (
-    <div>
-      操作员列表
+    <div className='user'>
+      <div className='flex-box'>
+        <Button type='primary' onClick={() => handClink('add')} style={{marginLeft:'1%'}}> + 新增 </Button>
+        <Form
+          layout='inline'
+          onFinish={hangFinish}
+        >
+          <Form.Item name="keyword">
+            <Input placeholder='请输入操作员'/>
+          </Form.Item>
+          <Form.Item name="keyword">
+            <Button type='primary' htmlType='submit' icon={<SearchOutlined />}> 查询 </Button>
+          </Form.Item>
+        </Form>
+      </div>
+      <Table columns={ column } dataSource={ data } rowKey={ data => data.key }/>
     </div>
   )
 }
--- src/pages/page/page.jsx
+++ src/pages/page/page.jsx
@@ -2,16 +2,18 @@
  * @Description: 
  * @Author: wangyonghong
  * @Date: 2024-09-29 14:23:17
- * @LastEditTime: 2024-09-30 14:49:29
+ * @LastEditTime: 2024-10-04 23:24:53
  */
-import React, { Component } from 'react'
+import React from 'react'
+import { Empty } from "antd";
 
-export default class Page extends Component {
-  render() {
-    return (
-      <div>
-        这是首页
+export default function Page() {
+  return (
+    <div style={{height:'100%'}}>
+      <div style={{paddingTop:'300px'}}>
+        <Empty />
+        <div>敬请期待...</div>
       </div>
-    )
-  }
+    </div>
+  )
 }
