916fa294a851c242117d9074ee28eee9f9f607ee
wyh20240819 <wangyonghong@byaidata.com>
2024-11-13 14:41:16 +0800

    配置代理

---
+++ src/setupProxy.js
@@ -0,0 +1,20 @@
+/*
+ * @Description: 配置代理
+ * @Author: wangyonghong
+ * @Date: 2024-11-13 13:58:29
+ * @LastEditTime: 2024-11-13 14:33:05
+ */
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+      '/api', // 匹配请求前缀为 /api 的请求
+      createProxyMiddleware({
+        target: 'http://localhost:3003', // 目标服务器地址
+        changeOrigin: true, // 将请求头中的 Origin 设置为目标 URL
+        pathRewrite: {
+          '^/api': '', // 去除路径中的 /api 前缀
+        },
+      })
+    );
+  };
--- src/utils/networkUrl.js
+++ src/utils/networkUrl.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: wangyonghong
  * @Date: 2024-11-13 09:51:20
- * @LastEditTime: 2024-11-13 09:53:14
+ * @LastEditTime: 2024-11-13 13:49:26
  */
 
-export const BASE = 'http://localhost:3003'
+export const BASE = 'http://localhost:3000'
