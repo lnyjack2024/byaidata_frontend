6465a0dafa7badce8953ea517ddd8769fd97a547
wyh20240819 <wangyonghong@byaidata.com>
2024-11-15 16:13:17 +0800

    不同环境请求配置信息

--- src/utils/networkUrl.js
+++ src/utils/networkUrl.js
@@ -2,7 +2,14 @@
  * @Description: 
  * @Author: wangyonghong
  * @Date: 2024-11-13 09:51:20
- * @LastEditTime: 2024-11-14 16:33:03
+ * @LastEditTime: 2024-11-15 16:11:32
  */
 
+//开发环境
 export const BASE = 'http://localhost:3003'
+
+// //测试环境
+// export const BASE = 'http://47.116.221.126:3004'
+
+// //生产环境
+// export const BASE = ''
