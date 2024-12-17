d7f6d4cc782f8dc8c0ff7e0795091777a59af905
wyh20240819 <wangyonghong@byaidata.com>
2024-12-17 13:35:49 +0800

    配置https

--- src/utils/networkUrl.js
+++ src/utils/networkUrl.js
@@ -2,14 +2,14 @@
  * @Description: 
  * @Author: wangyonghong
  * @Date: 2024-11-13 09:51:20
- * @LastEditTime: 2024-12-05 14:44:24
+ * @LastEditTime: 2024-12-17 13:32:50
  */
 
 //开发环境
 // export const BASE = 'http://localhost:3004'
 
 //测试环境
-export const BASE = 'http://47.116.221.126:3004'
+export const BASE = 'http://47.116.221.126:3004/api'
 
 // //生产环境
 // export const BASE = ''
