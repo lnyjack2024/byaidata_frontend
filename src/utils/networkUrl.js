6cabac2ae24ca9ed8b53c5c704706b80eaefee3a
wyh20240819 <wangyonghong@byaidata.com>
2024-12-25 13:17:56 +0800

    测试环境环境请求配置

--- src/utils/networkUrl.js
+++ src/utils/networkUrl.js
@@ -2,15 +2,10 @@
  * @Description: 
  * @Author: wangyonghong
  * @Date: 2024-11-13 09:51:20
- * @LastEditTime: 2024-12-20 12:14:26
+ * @LastEditTime: 2024-12-25 13:17:27
  */
-
-//开发环境
-// export const BASE = 'http://localhost:3004'
 
 //测试环境
 // export const BASE = 'https://47.116.221.126:3004'
 export const BASE = 'https://www.test.byaidata.com:3004'
 
-// //生产环境
-// export const BASE = ''
