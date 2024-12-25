dafedd331c77b36b489df0cb53f98402e6265dc9
wyh20240819 <wangyonghong@byaidata.com>
2024-12-25 13:16:37 +0800

    生产环境请求配置

--- src/utils/networkUrl.js
+++ src/utils/networkUrl.js
@@ -2,14 +2,8 @@
  * @Description: 
  * @Author: wangyonghong
  * @Date: 2024-11-13 09:51:20
- * @LastEditTime: 2024-12-13 14:46:04
+ * @LastEditTime: 2024-12-25 13:15:29
  */
 
-//开发环境
-// export const BASE = 'http://localhost:3004'
-
-//测试环境
-// export const BASE = 'http://47.116.221.126:3004'
-
 //生产环境
-export const BASE = ''
+export const BASE = 'http://www.byaidata.com:3004'
