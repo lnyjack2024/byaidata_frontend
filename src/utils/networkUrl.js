75117879786244a21d420d663572e185f2984c66
wyh20240819 <wangyonghong@byaidata.com>
2024-12-30 17:58:46 +0800

    生产环境-bug优化

--- src/utils/networkUrl.js
+++ src/utils/networkUrl.js
@@ -2,8 +2,8 @@
  * @Description: 
  * @Author: wangyonghong
  * @Date: 2024-11-13 09:51:20
- * @LastEditTime: 2024-12-26 10:57:49
+ * @LastEditTime: 2024-12-30 17:57:55
  */
 
 //生产环境
-export const BASE = 'http://www.byaidata.com:3004'
+export const BASE = 'https://www.byaidata.com:3004'
