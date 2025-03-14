3e001836d1d8ddef0fa3b5676bc45cffed52e609
wyh20240819 <wangyonghong@byaidata.com>
2025-03-14 16:40:39 +0800

    设置退出登录弹框

--- src/utils/networkUrl.js
+++ src/utils/networkUrl.js
@@ -2,8 +2,8 @@
  * @Description: 
  * @Author: wangyonghong
  * @Date: 2024-11-13 09:51:20
- * @LastEditTime: 2025-01-06 13:26:40
+ * @LastEditTime: 2025-01-06 13:25:17
  */
 
-//生产环境
+//测试环境
 export const BASE = 'https://www.byaidata.com:3004'
