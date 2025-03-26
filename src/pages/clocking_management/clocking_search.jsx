f6ed5e0d8997e4e63bedb4aeacb1b12be8407770
wyh20240819 <wangyonghong@byaidata.com>
2025-03-26 11:25:16 +0800

    考勤模块迭代

--- src/pages/clocking_management/clocking_search.jsx
+++ src/pages/clocking_management/clocking_search.jsx
@@ -2,11 +2,11 @@
  * @Description: 考勤数据查询
  * @Author: wangyonghong
  * @Date: 2025-03-19 15:51:34
- * @LastEditTime: 2025-03-20 16:16:55
+ * @LastEditTime: 2025-03-21 15:37:52
  */
 import React, { useEffect, useState } from 'react'
-import { SearchOutlined, VerticalAlignBottomOutlined, RedoOutlined } from '@ant-design/icons';
-import { Button, Form, Input, Table, Col, Row, DatePicker } from 'antd'
+import { SearchOutlined, VerticalAlignBottomOutlined, RedoOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Table, Col, Row, DatePicker, Tooltip } from 'antd'
 import dayjs from 'dayjs';
 import '../common_css/style.css'
 import { BASE } from '../../utils/networkUrl'
@@ -69,174 +69,181 @@ const ClockinSearch = () => {
 
   const column = [
     {
-      title: '姓名',
-      dataIndex: 'name',
-      fixed: 'left'
-    },
-    {
       title: '年月',
       dataIndex: 'years',
       fixed: 'left'
     },
     {
+      title: '姓名',
+      dataIndex: 'name',
+      fixed: 'left'
+    },
+    {
+      title: '基地',
+      dataIndex: 'base',
+    },
+    {
+      title: '组长',
+      dataIndex: 'group_manager',
+    },
+    {
       title: '项目-任务包',
       dataIndex: 'item_task',
-      fixed: 'left'
+    },
+    {
+      title: '应出勤天数',
+      dataIndex: 'planned_work_days',
+    },
+    {
+      title: '实际出勤天数',
+      dataIndex: 'planned_work_days',
+    },
+    {
+      title: '请假调休天数',
+      dataIndex: 'planned_work_days',
+    },
+    {
+      title: '加班总工时',
+      dataIndex: 'planned_work_days',
+    },
+    {
+      title: '培训期天数',
+      dataIndex: 'planned_work_days',
+    },
+    {
+      title: (
+        <span>
+          <Tooltip title="非入职和已离职的天数总和">
+            <InfoCircleOutlined />
+          </Tooltip>
+          &nbsp;在项天数
+        </span>
+      ),
+      dataIndex: 'planned_work_days',
     },
     {
       title: '1号',
       dataIndex: 'day_1',
-      width: 80
     },
     {
       title: '2号',
       dataIndex: 'day_2',
-      width: 80
     },
     {
       title: '3号',
       dataIndex: 'day_3',
-      width: 80
     },
     {
       title: '4号',
       dataIndex: 'day_4',
-      width: 80
     },
     {
       title: '5号',
       dataIndex: 'day_5',
-      width: 80
     },
     {
       title: '6号',
       dataIndex: 'day_6',
-      width: 80
     },
     {
       title: '7号',
       dataIndex: 'day_7',
-      width: 90
     },
     {
       title: '8号',
       dataIndex: 'day_8',
-      width: 80
     },
     {
       title: '9号',
       dataIndex: 'day_9',
-      width: 80
     },
     {
       title: '10号',
       dataIndex: 'day_10',
-      width: 80
     },
     {
       title: '11号',
       dataIndex: 'day_11',
-      width: 80
     },
     {
       title: '12号',
       dataIndex: 'day_12',
-      width: 80
     },
     {
       title: '13号',
       dataIndex: 'day_13',
-      width: 80
     },
     {
       title: '14号',
       dataIndex: 'day_14',
-      width: 80
     },
     {
       title: '15号',
       dataIndex: 'day_15',
-      width: 80
     },
     {
       title: '16号',
       dataIndex: 'day_16',
-      width: 80
     },
     {
       title: '17号',
       dataIndex: 'day_17',
-      width: 80
     },
     {
       title: '18号',
       dataIndex: 'day_18',
-      width: 80
     },
     {
       title: '19号',
       dataIndex: 'day_19',
-      width: 80
     },
     {
       title: '20号',
       dataIndex: 'day_20',
-      width: 80
     },
     {
       title: '21号',
       dataIndex: 'day_21',
-      width: 80
     },
     {
       title: '22号',
       dataIndex: 'day_22',
-      width: 80
     },
     {
       title: '23号',
       dataIndex: 'day_23',
-      width: 80
     },
     {
       title: '24号',
       dataIndex: 'day_24',
-      width: 80
     },
     {
       title: '25号',
       dataIndex: 'day_25',
-      width: 80
     },
     {
       title: '26号',
       dataIndex: 'day_26',
-      width: 80
     },
     {
       title: '27号',
       dataIndex: 'day_27',
-      width: 80
     },
     {
       title: '28号',
       dataIndex: 'day_28',
-      width: 80
     },
     {
       title: '29号',
       dataIndex: 'day_29',
-      width: 80
     },
     {
       title: '30号',
       dataIndex: 'day_30',
-      width: 80
     },
     {
       title: '31号',
       dataIndex: 'day_31',
-      width: 80
     }
   ];
 
